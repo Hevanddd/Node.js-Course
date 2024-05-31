180571e06c49f67d1d43abdd0af8fdcb14cdc8b9
Igor_Mandziak <Igor_Mandziak@epam.com>
2024-05-31 23:10:01 +0300

    add module 10

--- module-6/src/middlewares/isAdmin.middlware.ts
+++ module-6/src/middlewares/isAdmin.middlware.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { logger } from "../logger";
 
 export function isAdmin(req: Request, res: Response, next: NextFunction) {
   const currentUser = req.user;
 
   if (currentUser?.role !== "admin") {
+    logger.error("Only admins can do it");
     return res.status(403).send({
       data: null,
       error: {
